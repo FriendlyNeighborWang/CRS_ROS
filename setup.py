b03469298d5262150a25cb351b094a7d09b90a8e
wsh <wsh20040722@outlook.com>
2025-12-07 13:29:34 +0800

    init

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+
+package_name = 'maze_bot_sim'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'models'), glob('models/*')),
+        (os.path.join('share', package_name, 'worlds'), glob('worlds/*')),
+        (os.path.join('share', package_name, 'maps'), glob('maps/*')),
+        (os.path.join('share', package_name, 'params'), glob('params/*')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='yahboom',
+    maintainer_email='yahboom@todo.todo',
+    description='Maze Bot Simulation Package',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'mission_commander = maze_bot_sim.mission_commander:main',
+            'debug_stage2 = maze_bot_sim.debug_stage2:main',
+        ],
+    },
+)
